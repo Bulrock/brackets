8029983557daacb720baceb4830269ebb7a752c9
Bulrock <maksim.volozhinskij@gmail.com>
2022-07-27 23:52:44 +0300

    feat: implement check brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsMap = {};
+  bracketsConfig.forEach(element => {
+      bracketsMap[element[0]] = element[1];
+  });
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+
+    var currentBracket = str[i];
+    var lastOpeningBracket = stack[stack.length - 1];
+    if (bracketsMap[lastOpeningBracket] === currentBracket){
+      stack.pop()
+    } 
+    else {
+      stack.push(currentBracket);
+    }
+  }
+
+  return stack.length === 0;
 }
